f2f6c799a086b777d5af8ee2099f50594b6edfd2
IgnacioCon <contrerasign@gmail.com>
2018-11-01 20:58:53 -0600

    Modified frontend of the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   has_one_attached :avatar
+  validate :has_image_attached
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+  def has_image_attached
+    if !avatar.attached?
+      errors.add(:avatar, "Please upload a picture!")
+    end
+  end
+         
 end
